d3b8c7deeeb0edd8ce6d1537058210f86718c45a
Luo Donna <luodonna@Pei-wen.local>
2020-07-17 12:43:25 +0100

    week4_HW_R

--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -1,27 +1,27 @@
-/* eslint-disable */
-
+// eslint-disable-next-line
 const request = require('request');
+// eslint-disable-next-line
 const API_ENDPOINT = 'https://restcountries.eu/rest/v2/name/';
-const process = require('process')
+// eslint-disable-next-line
+const process = require('process');
 request(`${API_ENDPOINT}${process.argv[2]}`, (err, response, body) => {
-    if (err) {
-        return console.log('抓取失敗', err);
-    };
-    if (response.statusCode === 404) {
-      console.log('找不到國家資訊');
-      return
-    };
-    let data;
-    try {
-      data = JSON.parse(body);
-    } catch(err) {
-      console.log(err.name);
-      console.log(err.message)
-      return
-    };
+  if (err) {
+    console.log('抓取失敗', err);
+    return;
+    // eslint-disable-next-line
+  }
+  if (response.statusCode === 404) {
+    console.log('找不到國家資訊');
+    // eslint-disable-next-line
+    return;
+  }
+  const data = JSON.parse(body);
+  for (let i = 0; i < data.length; i += 1) {
     console.log('statusCode:', response && response.statusCode);
-    console.log(`國家: ${data[0].name}`);
-    console.log(`首都: ${data[0].capital}`);
-    console.log('貨幣：' + data[0].currencies[0].code);
-    console.log(`國碼：${data[0].callingCodes}`); 
+    console.log(`國家: ${data[i].name}`);
+    console.log(`首都: ${data[i].capital}`);
+    console.log(`貨幣：${data[i].currencies[0].code}`);
+    console.log(`國碼：${data[i].callingCodes}`);
+    console.log('=============');
+  }
 });
